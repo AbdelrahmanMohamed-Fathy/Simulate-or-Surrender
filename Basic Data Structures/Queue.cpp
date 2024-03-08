395e512f8183af2639f7fc713dd0d136795f2121
Abdelrahman Fathy <abdelrahman.fathy04@outlook.com>
2024-03-08 18:26:57 +0200

    finished queue implementation

--- Basic Data Structures/Queue.cpp
+++ Basic Data Structures/Queue.cpp
@@ -10,29 +10,51 @@ bool Queue<T>::isEmpty() const
 template <class T>
 void Queue<T>::enqueue(const T &newEntry)
 {
-    Node<T>* TempHolder = new Node<T>
+    Node<T>* TempHolder = new Node<T>;
     TempHolder->Data = newEntry;
     if(isEmpty())
     {
-        Head=TempHolder;
+        Head = TempHolder;
         return;
     }
-    
+    Tail->Next = TempHolder;
+    Tail = TempHolder;
 }
 
 template <class T>
 bool Queue<T>::dequeue(T &FrontEntry)
 {
-    return false;
+    if (isEmpty())
+    {
+        FrontEntry = NULL;
+        return false;
+    }
+    FrontEntry = Head->Data;
+    Node<T>* TempHolder = Head;
+    Head = Head->Next;
+    delete TempHolder;
+    return true;
 }
 
 template <class T>
 bool Queue<T>::peek(T &FrontEntry) const
 {
-    return false;
+    if (isEmpty())
+    {
+        FrontEntry = NULL;
+        return false;
+    }
+    FrontEntry = Head->Data;
+    return true;
 }
 
 template <class T>
 Queue<T>::~Queue()
 {
+    while(Head)
+    {
+        Node<T>* TempHolder = Head->Next;
+        delete Head;
+        Head = TempHolder;
+    }
 }
