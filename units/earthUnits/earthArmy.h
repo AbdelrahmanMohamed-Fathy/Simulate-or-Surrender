2d106cfcf9fd43bf4eb77a34cef6a577f12f7641
Abdelrahman Fathy <abdelrahman.fathy04@outlook.com>
2024-03-27 19:01:01 +0200

    managers working again

--- units/earthUnits/earthArmy.h
+++ units/earthUnits/earthArmy.h
@@ -2,6 +2,9 @@
 #include "../../basicDataStructures/queue.h"
 #include "../../basicDataStructures/stack.h"
 #include "../../basicDataStructures/priQueue.h"
+#include "humanSoldier/humanSoldier.h"
+#include "humanTank/humanTank.h"
+#include "humanGunner/humanGunner.h"
 
 class gameManager;
 
@@ -9,17 +12,17 @@ class earthArmy
 {
 private:
 	gameManager* gm;
-	queue<>* soldiers;
-	stack<>* tanks;
-	priQueue<>* gunners;
+	queue<humanSoldier*>* soldiers;
+	stack<humanTank*>* tanks;
+	priQueue<humanGunner*>* gunners;
 	unsigned int nextFreeID;
 public:
 	//earthArmy();
 
 	//Getters:
-	queue<>* getSoldiers();
-	stack<>* getTanks();
-	priQueue<>* getGunners();
+	queue<humanSoldier*>* getSoldiers();
+	stack<humanTank*>* getTanks();
+	priQueue<humanGunner*>* getGunners();
 
 	//Adders:
 	void addSoldier();
