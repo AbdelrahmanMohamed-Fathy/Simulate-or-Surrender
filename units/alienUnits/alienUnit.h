01000a3f1eaf9014f3bef05546a3749cfafb4ce5
Abdelrahman Fathy <abdelrahman.fathy04@outlook.com>
2024-04-03 01:39:36 +0200

    edited attack

--- units/alienUnits/alienUnit.h
+++ units/alienUnits/alienUnit.h
@@ -7,6 +7,6 @@ class alienUnit : public unit_Interface
 {
 public:
 	alienUnit(int iD, int Hp, int Pr, int Ac, int Tj) : unit_Interface(iD, Hp, Pr, Ac, Tj) {};
-	//virtual void attack(earthArmy* humans, bool printed) = 0;
+	//virtual void attack(earthArmy* humans, int timeStep, bool printed) = 0;
 };
 
