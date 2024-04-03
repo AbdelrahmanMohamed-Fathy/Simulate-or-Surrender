32facd8bed79a0a4e8f5711d638ce4a16a307ae0
Abdelrahman Fathy <abdelrahman.fathy04@outlook.com>
2024-04-03 11:41:35 +0200

    add count-- to dequeue

--- basicDataStructures/deQueue.cpp
+++ basicDataStructures/deQueue.cpp
@@ -65,14 +65,14 @@ bool deQueue<T>::dequeuefront(T& front)
 {
 	if (isEmpty())
 	{
-		front = NULL;
 		return false;
 	}
 	front = Head->data;
 	doublyNode<T>* delf = Head;
 	Head = Head->next;
-	Head->prev = NULL;
+	Head->prev = nullptr;
 	delete delf;
+	count--;
 	return true;
 }
 
@@ -81,14 +81,14 @@ bool deQueue<T>::dequeueback(T& back)
 {
 	if (!Tail)
 	{
-		back = NULL;
 		return false;
 	}
 	back = Tail->data;
 	doublyNode<T>* delb = Tail;
 	Tail = Tail->prev;
-	Tail->next = NULL;
+	Tail->next = nullptr;
 	delete delb;
+	count--;
 	return true;
 }
 
@@ -125,15 +125,13 @@ void deQueue<T>::print()
 template<typename T>
 bool deQueue<T>::enqueue(const T& newEntry)
 {
-	enqueueBack(newEntry);
-	return false;
+	return enqueueBack(newEntry);
 }
 
 template<typename T>
 bool deQueue<T>::dequeue(T& FrontEntry)
 {
-	dequeuefront(FrontEntry);
-	return false;
+	return dequeuefront(FrontEntry);
 }
 
 template<typename T>
