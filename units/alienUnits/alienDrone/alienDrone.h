3f8ccbd534f641dd8272befdbd8a46702e40d61d
Abdelrahman Sakr <abdelrahman.mansour04@eng-st.cu.edu.eg>
2024-04-20 18:45:30 +0200

    Gunner and Drone Attack complete

--- units/alienUnits/alienDrone/alienDrone.h
+++ units/alienUnits/alienDrone/alienDrone.h
@@ -5,5 +5,5 @@ class alienDrone : public alienUnit
 {
 public:
 	alienDrone(int iD, int Hp, int Pr, int Ac, int Tj) : alienUnit(iD, Hp, Pr, Ac, Tj) {};
-	void attack(earthArmy* humans, int timeStep, bool printed);
+	void attack(earthArmy* humans, queue<unit_Interface*>* deathList, int timeStep, bool printed);
 };
