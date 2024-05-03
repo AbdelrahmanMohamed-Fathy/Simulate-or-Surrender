3d6f49379506d67029107fa1b25b2b952709ba18
Abdelrahman Fathy <abdelrahman.fathy04@outlook.com>
2024-05-03 15:58:23 +0300

    added:
    addtoMaintenance function inside gamemanager
    checkMaintenanceListViability inside earthUnit

--- units/earthUnits/earthUnit.cpp
+++ units/earthUnits/earthUnit.cpp
@@ -4,3 +4,10 @@ void earthUnit::setMaintenanceWaitStartTime(int timeStep)
 {
 	maintenanceWaitStartTime = timeStep;
 }
+
+bool earthUnit::checkMaintenanceListViability()
+{
+	if (health < maxHealth * 0.2) 
+		return true;
+	return false;
+}
